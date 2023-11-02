f2a739f9a5bfdf138805da51ec1e7f2cfeffd8e1
Jules Delamare <jdelamare@cegid.com>
2023-11-02 15:14:33 +0100

    basic column reading

--- pock8-testing/Program.cs
+++ pock8-testing/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI.ChatCompletion;
+using System.Text;
+using System.Text.Json;
+
+// https://devblogs.microsoft.com/dotnet/demystifying-retrieval-augmented-generation-with-dotnet/
+
+fstring aoaiEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")!;
+string aoaiApiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")!;
+string aoaiModel = "gpt-35-turbo";
+
+// Initialize the kernel
+IKernel kernel = Kernel.Builder
+    .WithLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+    .WithAzureChatCompletionService(aoaiModel, aoaiEndpoint, aoaiApiKey)
+    .Build();
+
+// Create a new chat
+IChatCompletion ai = kernel.GetService<IChatCompletion>();
+ChatHistory chat = ai.CreateNewChat("you are trying to read a CSV");
+StringBuilder builder = new();
+
+string settingsString = File.ReadAllText("ressources/settings.json");
+IEnumerable<String> contentExample = File.ReadLines("ressources/example1.csv").Take(5);
+
+JsonElement settings = (JsonElement) JsonSerializer.Deserialize<object>(settingsString);
+
+StringBuilder data = new();
+foreach (string line in contentExample)
+{
+    data.AppendLine(line);
+}
+chat.AddUserMessage("you have these data : " + data.ToString());
+
+foreach(object info in settings.GetProperty("helpInfo").EnumerateArray())
+{
+    chat.AddUserMessage(info.ToString());
+}
+
+chat.AddUserMessage("match these column name with the of the CSV : " + settings.GetProperty("possibleColumns").ToString());
+chat.AddUserMessage("i want your answer in this format : " + settings.GetProperty("outputFormat").ToString());
+
+builder.Clear();
+await foreach (string message in ai.GenerateMessageStreamAsync(chat))
+{
+    builder.Append(message);
+}
+chat.AddAssistantMessage(builder.ToString());
+
+try
+{
+    JsonElement answer = (JsonElement)JsonSerializer.Deserialize<object>(builder.ToString());
+    Console.WriteLine(answer.ToString());
+}
+catch(Exception e)
+{
+    Console.WriteLine(builder.ToString());
+}
+
+builder.Clear();
+
